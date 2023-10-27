3bc5f6058ba16bb7214147df19376c6f9ea1f22c
joseporresD <jose.porres@devmente.com>
2023-10-26 19:56:59 -0500

    feat: database crud with concurrency control

--- app/crud.py
+++ app/crud.py
@@ -1,6 +1,24 @@
 from sqlalchemy.orm import Session
 from app import models, schemas
 
+##################################################################
+############## PRIVATE FUNCTIONS #################################
+##################################################################
+def add_item(db: Session, item: schemas.Item):
+    db_item = models.Item(id=item.id, quantity=item.quantity)
+    db.add(db_item)
+    db.commit()
+    db.refresh(db_item)
+    return db_item
+
+##################################################################
+##################################################################
+##################################################################
+
+##################################################################
+############## SIMPLE CRUD #######################################
+##################################################################
+
 def get_item(db: Session, itemId: str = None, page: int = 1, size: int = 10):
     skip = (page - 1) * size
     query = db.query(models.Item)
@@ -19,6 +37,9 @@ def get_item(db: Session, itemId: str = None, page: int = 1, size: int = 10):
         "size": size
     }
 
+def get_all_items(db: Session):
+    return db.query(models.Item).all()
+
 def buy_item(db: Session, item: schemas.Item):
     db_item = db.query(models.Item).filter(models.Item.id == item.id).first()
     if db_item:
@@ -29,14 +50,43 @@ def buy_item(db: Session, item: schemas.Item):
     else:
         return None
     
-def add_item(db: Session, item: schemas.Item):
-    db_item = models.Item(id=item.id, quantity=item.quantity)
-    db.add(db_item)
-    db.commit()
-    db.refresh(db_item)
-    return db_item
-    
+##################################################################
+##################################################################
+##################################################################
 
-def get_all_items(db: Session):
-    return db.query(models.Item).all()
+
+##################################################################
+############## Concurrency Control ###############################
+##################################################################
     
+def buy_item_optimistic_locking(db: Session, item: schemas.Item):
+    db_item = db.query(models.Item).filter(models.Item.id == item.id).first()
+    if db_item:
+        if db_item.quantity >= item.quantity:
+            db_item.quantity -= item.quantity
+            db_item.version += 1  # Increment the version to indicate the change
+            db.commit()
+            db.refresh(db_item)
+            return db_item
+        else:
+            raise Exception("Not enough items in stock")
+    else:
+        return None
+    
+def buy_item_pessimistic_locking(db: Session, item: schemas.Item):
+    db_item = db.query(models.Item).filter(models.Item.id == item.id).with_for_update().first()
+    if db_item:
+        if db_item.quantity >= item.quantity:
+            db_item.quantity -= item.quantity
+            db.commit()
+            db.refresh(db_item)
+            return db_item
+        else:
+            raise Exception("Not enough items in stock")
+    else:
+        return None
+    
+##################################################################
+##################################################################
+##################################################################
+
